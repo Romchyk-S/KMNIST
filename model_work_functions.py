8b3ed8faaba5b05cea5d1fa27d60db790ce45220
Roman Ščerbak <roma.shcherbak1205@gmail.com>
2024-09-12 17:44:18 +0300

    Torch on GPU, languages to files, lr scheduler, F1
    Ability to move torch model to GPU if available, sent language text to separate files, fixed up plot_learning_curve titles and filenames (not tested yet), added f1_score to keras model (not tested yet), added plotting to torch models (not tested yet), added learning rate scheduler to torch model (also not tested yet).

--- model_work_functions.py
+++ model_work_functions.py
@@ -10,6 +10,7 @@ import os as os
 import tensorflow.keras.layers as tkl
 import tensorflow.keras.losses as tklosses
 import tensorflow.keras.models as tkm
+import torch.optim.lr_scheduler as lr_scheduler
 import time as tm
 import numpy as np
 import torch as torch
@@ -18,6 +19,7 @@ import torch.optim as optim
 import torch.utils.data as tudata
 import nn_torch_class as nntc
 import load_plot_data as lpd
+import sklearn.metrics as skmetrics
 
 def find_model_amount(library: str) -> int:
     try:
@@ -32,16 +34,18 @@ def find_model_amount(library: str) -> int:
         models_built_amount = 0
     return models_built_amount
 
-def plot_learning_curve(metric: str, epoch_range: list, history: list, models_built_amount: int):    
+def plot_learning_curve(metric: str, epoch_range: list, history: list, models_built_amount: int, library: str):    
    
+    # adapt for plotting train and test curves on the same plot
     plt.plot(epoch_range, history[metric])
-    plt.title(f'training_{metric} for keras model_{models_built_amount}')
+    plt.title(f'training_{metric} for {library} model_{models_built_amount}')
     plt.xlabel('epochs')
     plt.ylabel(f'{metric}')   
-    plt.savefig(f'./plots/{metric}_keras_model_{models_built_amount}')
+    plt.savefig(f'./plots/{metric} {library} model_{models_built_amount}')
     plt.show()
 
-def build_keras_model(X_train, Y_train, X_test, Y_test, filepath: str, models_built_amount: int, epochs: int, kernel_size: tuple, pool_size: tuple, classes_amount: int, batch_size: int):
+def build_keras_model(X_train, Y_train, X_test, Y_test, filepath: str, models_built_amount: int, epochs: int, 
+                      kernel_size: tuple, pool_size: tuple, classes_amount: int, batch_size: int, optimizer: str = 'adam'):
     
     model = tkm.Sequential([    
     tkl.Input(X_train.shape[1:4]),
@@ -60,7 +64,8 @@ def build_keras_model(X_train, Y_train, X_test, Y_test, filepath: str, models_bu
      tkl.Dense(16, activation='relu'),
      tkl.Dense(classes_amount)])
 
-    model.compile(loss = tklosses.SparseCategoricalCrossentropy(from_logits = True), optimizer='adam', metrics=['accuracy'])
+    # add more metrics
+    model.compile(loss = tklosses.SparseCategoricalCrossentropy(from_logits = True), optimizer=optimizer, metrics=['accuracy', skmetrics.f1_score])
     
     print("Training the model")
     start = tm.perf_counter()
@@ -68,12 +73,11 @@ def build_keras_model(X_train, Y_train, X_test, Y_test, filepath: str, models_bu
     history = history.history
     epoch_range = list(range(epochs))
     epoch_range = list(map(lambda x: x+1, epoch_range))
-    plot_learning_curve('accuracy', epoch_range, history, models_built_amount)
-    plot_learning_curve('loss', epoch_range, history, models_built_amount)
+    plot_learning_curve('accuracy', epoch_range, history, models_built_amount, 'keras')
+    plot_learning_curve('loss', epoch_range, history, models_built_amount, 'keras')
     
     print(f"Training takes: {tm.perf_counter()-start} seconds.")
     print()
-    
     print("Model accuracy on the test set")
     model.evaluate(X_test, Y_test) # test the CNN  
     
@@ -89,20 +93,26 @@ def build_keras_model(X_train, Y_train, X_test, Y_test, filepath: str, models_bu
     
     return model
 
-def build_torch_model(X_train, Y_train, X_test, Y_test, filepath: str, epochs: int,  kernel_size: tuple, pool_size: tuple, classes_amount: int,  batch_size: int):
+def build_torch_model(X_train, Y_train, X_test, Y_test, filepath: str, epochs: int,  
+                      kernel_size: tuple, pool_size: tuple, classes_amount: int, 
+                      batch_size: int, models_built_amount: int, device: str):
+    
+    train_loss_history, train_acc_history, test_loss_history, test_acc_history = [], [], [], []
+    epoch_range = list(range(epochs))
+    epoch_range = list(map(lambda x: x+1, epoch_range))
     
     input_channels = X_train.shape[3]
-    model = nntc.NN_torch(pool_size, kernel_size, classes_amount, input_channels)
+    model = nntc.NN_torch(pool_size, kernel_size, classes_amount, input_channels).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters())
 
-    X_train = X_train.reshape(X_train.shape[0], X_train.shape[3], X_train.shape[1], X_train.shape[2])
+    X_train = X_train.reshape(X_train.shape[0],X_train.shape[3],X_train.shape[1],X_train.shape[2])
     X_train = torch.tensor(X_train, dtype=float)
     Y_train = torch.tensor(Y_train, dtype=float)
     Y_train = Y_train.type(torch.LongTensor)
     
     dataset = tudata.TensorDataset(X_train, Y_train)
-    X_test = X_test.reshape(X_test.shape[0], X_test.shape[3],X_test.shape[1], X_test.shape[2])
+    X_test = X_test.reshape(X_test.shape[0],X_test.shape[3],X_test.shape[1],X_test.shape[2])
     X_test = torch.tensor(X_test, dtype=float)
     Y_test = torch.tensor(Y_test, dtype=float)
     Y_test = Y_test.type(torch.LongTensor)
@@ -114,8 +124,7 @@ def build_torch_model(X_train, Y_train, X_test, Y_test, filepath: str, epochs: i
                                                   batch_size=batch_size, shuffle = False)
     
     start = tm.perf_counter()
-    
-    # LR scheduler
+    scheduler = lr_scheduler.LinearLR(optimizer, start_factor = 1.0, end_factor = 0.5, total_iters = epochs-2)
     
     for epoch in range(epochs):  # loop over the dataset multiple times
     
@@ -127,6 +136,9 @@ def build_torch_model(X_train, Y_train, X_test, Y_test, filepath: str, epochs: i
         for (i, data) in enumerate(training_loader):
             
             x, y = data 
+            
+            x = x.to(device)
+            y = y.to(device)
             optimizer.zero_grad()
             outputs = model(x)
             _, predicted = torch.max(outputs, 1)
@@ -137,13 +149,14 @@ def build_torch_model(X_train, Y_train, X_test, Y_test, filepath: str, epochs: i
             loss.backward()
             optimizer.step()
             running_loss_train += loss.item()
-            
-            # test evaluation
         
         print(f"Time taken to train on epoch {epoch}: {round(tm.perf_counter()-start_epoch,3)} seconds")
-        print(f"Loss: {round(running_loss_train/len(dataset), 3)}")
-        accuracy = round(100 * (total_correct / total_samples), 3)
-        print(f"Accuracy: {accuracy}%")
+        train_loss_epoch = running_loss_train/len(dataset)
+        print(f"Loss: {round(train_loss_epoch, 3)}")
+        train_loss_history.append(train_loss_epoch)
+        train_accuracy_epoch = 100 * (total_correct / total_samples)
+        print(f"Accuracy: {round(train_accuracy_epoch,3)}%")
+        train_acc_history.append(train_accuracy_epoch)
         print()
             
     print(f"Time taken to train {round(tm.perf_counter()-start, 3)} seconds")
@@ -160,16 +173,28 @@ def build_torch_model(X_train, Y_train, X_test, Y_test, filepath: str, epochs: i
         running_loss_test += loss.item()
 
     print(f"Test_set_loss: {round(running_loss_test, 3)}")
-    accuracy = round(100 * (total_correct / total_samples), 3)
-    print(f"Accuracy on test set: {accuracy}")
+    test_loss_history.append(running_loss_test)
+    test_accuracy_epoch = 100 * (total_correct / total_samples)
+    print(f"Accuracy on test set: {round(test_accuracy_epoch, 3)}")
+    test_acc_history.append(test_accuracy_epoch)
+    
     print()
     torch.save(model, filepath)
+    
+    history = {'train_loss': train_loss_history, 'train_accuracy': train_acc_history,
+               'test_loss': test_loss_history, 'test_accuracy': test_acc_history}
+    
+    # find a way to plot on the same plot
+    plot_learning_curve('train_loss', epoch_range, history, models_built_amount, 'torch')
+    plot_learning_curve('train_accuracy', epoch_range, history, models_built_amount, 'torch')
+    plot_learning_curve('test_loss', epoch_range, history, models_built_amount, 'torch')
+    plot_learning_curve('test_accuracy', epoch_range, history, models_built_amount, 'torch')
+    scheduler.step()
     return model
     
 def make_and_plot_prediction(imgs, labels, indexes, model, classmap, elements_to_plot, model_name: str, dataset: str):
     
     imgs_to_predict = imgs[indexes]
-    
     try:
         prediction = model.predict(imgs_to_predict)
     except AttributeError:
